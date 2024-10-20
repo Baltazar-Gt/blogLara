55197f04df44a47047d0d66fa4131ca7e635aad8
Just_シャイ <161271889+Baltazar-Gt@users.noreply.github.com>
2024-10-19 23:41:53 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     url_api: 'https://pokeapi.co/api/v2/',
-    api: 'http://blogb-production.up.railway.app',
-    assets: 'http://blogb-production.up.railway.app'
+    api: 'http://localhost:8000/api/',
+    assets: 'http://localhost:8000/'
 };
